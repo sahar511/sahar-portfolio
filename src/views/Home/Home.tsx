e0fed326196147ccc758d2a5b6cfc0935c915c43
sahar511 <sahararef@sahars-Air.gateway.sasktel.net>
2024-05-18 09:53:37 -0600

    style: fix font-size

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className={`${styles.box} animate__animated animate__fadeIn`}>
       <h1 className={styles.title}>Sahar Arefzadeh</h1>
-      <h2 className={styles.subtitle}>Software Engineer(Front-End)</h2>
+      <h2 className={styles.subtitle}>Software Engineer</h2>
       <h4>
-        I am a software engineer with over four years of experience in front-end development working in cross-functional teams. My focus revolves around enhancing projects through feature development, and code maintenance. Proficient in React, Angular, and other web devllopement technologies.
+        I am a software engineer with 4+ years of experience in front-end development working in cross-functional teams. My focus revolves around enhancing projects through feature development and code maintenance. Proficient in React, Angular, and other web development technologies.
       </h4>
       <SocialLinks />
     </div>
